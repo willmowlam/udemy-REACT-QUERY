3d94551f1317ea6178aa805a45f721da68d86fbd
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-04-17 17:49:05 +0100

    Add error object details

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -8,7 +8,7 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, isError, isLoading } = useQuery({
+  const { data, isError, error, isLoading } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
   });
@@ -18,7 +18,12 @@ export function Posts() {
   }
 
   if (isError) {
-    return <h3>Oops, something went wrong!</h3>;
+    return (
+      <>
+        <h3>Oops, something went wrong!</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
   }
 
   return (

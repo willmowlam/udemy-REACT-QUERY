e28e7402a8c4c76d858f352f00c6971cf2857431
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-04-18 14:36:33 +0100

    Implement prefetching

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -7,6 +7,18 @@ const maxPostPage = 10;
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery({
+        queryKey: ["posts", nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }, [currentPage, queryClient]);
 
   const { data, isError, error, isLoading } = useQuery({
     queryKey: ["posts", currentPage],

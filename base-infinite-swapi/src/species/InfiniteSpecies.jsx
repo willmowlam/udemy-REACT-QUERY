d772baf8f690ba9074f0b859b2464cf888c0d9c2
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-04-19 20:36:54 +0100

    Show approprate messages for loading, fetching and errors

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -9,7 +9,7 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
+  const { data, fetchNextPage, hasNextPage, isFetching, isLoading, isError, error } = useInfiniteQuery({
     queryKey: ["sw-species"], // The name of the query cache key
     queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam), // The initial url and callback for query urls
     getNextPageParam: (lastPage) => {
@@ -17,25 +17,42 @@ export function InfiniteSpecies() {
     }
   });
 
-  // Map over each person within mapped pages
-  return <InfiniteScroll
-    loadMore={() => {
-      // prevent multi fetching
-      if (!isFetching) {
-        fetchNextPage();
-      }
-    }}
-    hasMore={hasNextPage}
-  >
-    {data.pages.map((pageData) => {
-      return pageData.results.map(species => {
-        return <Species
-          key={species.name}
-          name={species.name}
-          language={species.language}
-          averageLifespan={species.averageLifespan}
-        />
-      })
-    })}
-  </InfiniteScroll>;
+  if (isLoading) {
+    return <div className="loading">Loading...</div>
+  }
+
+  if (isError) {
+    return <div className="error">Error: {error.toString()}</div>
+  }
+
+  return <>
+    {
+      // Show during scroll
+      isFetching && <div className="loading">Loading...</div>
+    }
+    <InfiniteScroll
+      loadMore={() => {
+        // prevent multi fetching
+        if (!isFetching) {
+          fetchNextPage();
+        }
+      }}
+      hasMore={hasNextPage}
+    >
+      {
+        // Map over the pages data
+        data.pages.map((pageData) => {
+          // Map over the species data in a page
+          return pageData.results.map(species => {
+            // Return a species child component
+            return <Species
+              key={species.name}
+              name={species.name}
+              language={species.language}
+              averageLifespan={species.averageLifespan}
+            />
+          })
+        })}
+    </InfiniteScroll>
+  </>;
 }

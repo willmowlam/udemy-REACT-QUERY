79582dfabe8a904190fa1a41b598e8ad39a0c02c
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-04-19 20:25:05 +0100

    Prevent mutiple fetches and map over pages and species data

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,5 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useIsFetching } from "@tanstack/react-query";
 import { Species } from "./Species";
 
 const initialUrl = "https://swapi.dev/api/species/";
@@ -9,12 +9,33 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
     queryKey: ["sw-species"], // The name of the query cache key
     queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam), // The initial url and callback for query urls
     getNextPageParam: (lastPage) => {
       return lastPage.next || undefined;
     }
   });
-  return <InfiniteScroll />;
+
+  // Map over each person within mapped pages
+  return <InfiniteScroll
+    loadMore={() => {
+      // prevent multi fetching
+      if (!isFetching) {
+        fetchNextPage();
+      }
+    }}
+    hasMore={hasNextPage}
+  >
+    {data.pages.map((pageData) => {
+      return pageData.results.map(species => {
+        return <Species
+          key={species.name}
+          name={species.name}
+          language={species.language}
+          averageLifespan={species.averageLifespan}
+        />
+      })
+    })}
+  </InfiniteScroll>;
 }

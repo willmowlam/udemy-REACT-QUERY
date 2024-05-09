40cd61f2edd8faeecb46ebcc3f5f0a3dffe5e484
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-05-09 12:05:17 +0100

    Add empty fallback array for when data not yet available

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,8 +12,12 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
+
+  // Fallback with an empty Treatment array for when data is not (yet) available
+  const fallback: Treatment[] = [];
+
   // get data from server via useQuery
-  const { data } = useQuery({
+  const { data = fallback } = useQuery({
     queryKey: [queryKeys.treatments],
     queryFn: getTreatments,
   })
